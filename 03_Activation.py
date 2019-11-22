928bf838ce6f8a4ae4f7023d9ea7941358422cac
sangmin99554 <sangmin99554@naver.com>
2019-11-22 23:39:05 +0900

    add LeakyRELU

--- 03_Activation.py
+++ 03_Activation.py
@@ -1,13 +1,18 @@
 import numpy as np
+
 import matplotlib.pyplot as plt
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 def ReLU(x):
     return np.maximum(0,x)
-x = np.array([-5, 10, 1.,2.,3.,4.,5., 100])
+
+def LeakyRelu(x): # Leaky ReLU(Rectified Linear Unit, 정류된 선형 유닛) 함수
+    return np.maximum(0.01*x,x) # same
+x = np.array([-5, 10, 0, 1.,2.,3.,4.,5., 100])
 
 print("X data: ", x)
 y = sigmoid(x)
+y = LeakyRelu(x)
 # y = ReLU(x)
 print("After activation: ", y)
 
