eb31a2d7435a40c7845dc2ce1168d548717b5c7f
sangmain <sangmin99554@naver.com>
2019-12-30 15:01:29 +0900

    Euclidean DIstance

--- Euclidean/1.py
+++ Euclidean/1.py
@@ -1,45 +1,41 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("./3.jpg")
+img = cv2.imread("./2.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-_, bin_img = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY_INV)
+_, bin_img = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
 # cv2.imshow("aaa", dst)
 # cv2.waitKey(0)
 
 
-# kernel = np.ones((3, 3), np.uint8)
-# proc = cv2.erode(dst, kernel, iterations=1)
-# proc = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
+kernel = np.ones((3, 3), np.uint8)
+proc = cv2.erode(bin_img, kernel, iterations=1)
+proc = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
 
-# # cv2.imshow("aaa", a)
-# # cv2.waitKey(0)
-
-
-# proc = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
-# proc = cv2.erode(proc, kernel, iterations=1)
-
-# proc = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
-# bin_img = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
-
-# cv2.imshow("aaa", bin_img)
+# cv2.imshow("aaa", a)
 # cv2.waitKey(0)
 
-print("start")
+
+proc = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
+proc = cv2.erode(proc, kernel, iterations=1)
+
+proc = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
+bin_img = cv2.dilate(proc, kernel, iterations=1)  #// make dilation image
+
+
 ### prefix sum
 def prefix(array):
     num = 1
     result = []
 
     for i in range(array.shape[0]):
-        if array[i] == 1:
+        if array[i] == 255:
             result.append(num)
             num += 1
 
         else:
             result.append(0)
             num = 1
-
     return result
 
 ### postfix sum
@@ -49,7 +45,7 @@ def postfix(array):
     result = []
 
     for i in range(array.shape[0]):
-        if array[i] == 1:
+        if array[i] == 255:
             result.append(num)
             num += 1
 
@@ -60,8 +56,8 @@ def postfix(array):
     result = result[::-1]
     return result
  
-bin_img = bin_img.reshape(-1, bin_img.shape[0])
 print(bin_img.shape)
+
 row_pre= np.zeros((bin_img.shape)); row_post = np.zeros((bin_img.shape)); col_pre = np.zeros((bin_img.shape)); col_post = np.zeros((bin_img.shape));
 
 ### 열
@@ -69,15 +65,6 @@ for i in range(bin_img.shape[0]):
     row = bin_img[i, :]
     row_pre[i, :] = prefix(row)
     row_post[i, :] = postfix(row)
-    # print(bin_img[i, :])
-    print(prefix(row))
-    print()
-
-# for i in range(bin_img.shape[0]):
-#     for j in range(bin_img.shape[1]):
-#         if row_post[i, j] != 0.0:
-#             print(row_post[i,j])
-
 
 ### 비교
 row_result = np.zeros((bin_img.shape))
@@ -102,7 +89,6 @@ for i in range(bin_img.shape[0]):
         col_result[i,j] = min(col_pre[i,j], col_post[i,j])
 
 ### 비교
-
 result = np.zeros((bin_img.shape))
 for i in range(bin_img.shape[0]):
     for j in range(bin_img.shape[1]):
@@ -110,16 +96,18 @@ for i in range(bin_img.shape[0]):
             continue
         result[i,j] = min(row_result[i,j], col_result[i,j])
 
-print(result)
 
-# for i in range(bin_img.shape[0]):
-#     for j in range(bin_img.shape[1]):
-#         if bin_img[i, j] != 0:
-#             print(result[i,j])
+cv2.imshow("original binary image", bin_img)
 
-# cv2.imshow("result", result)
-# cv2.waitKey(0)
+cv2.normalize(result, result, 0, 1.0, cv2.NORM_MINMAX)
+cv2.imshow('Distance Transform Image', result)
 
 
 
+print()
 
+dist  = cv2.distanceTransform(bin_img,  cv2.DIST_L2, 3)
+
+cv2.normalize(dist, dist, 0, 1.0, cv2.NORM_MINMAX)
+cv2.imshow("result", dist)
+cv2.waitKey(0)
